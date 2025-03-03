c7b0b3c4d8f171ddb19b0327fbebcd1769cc574e
AngelikaJuhasz <angelikajuhasz.aj@gmail.com>
2025-03-04 00:02:07 +0100

    Visual Implementation 1
    Complete Map with Collision
    Begining of player movemnt anim

--- Assets/Scrpits/Player/PlayerMovement.cs
+++ Assets/Scrpits/Player/PlayerMovement.cs
@@ -2,27 +2,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float speed = 5f;
     private Rigidbody2D rb;
+    private Animator animator;
     private Vector2 movement;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        // Get Input
-        movement.x = Input.GetAxisRaw("Horizontal");  // A/D or Left/Right Arrow
-        movement.y = Input.GetAxisRaw("Vertical");    // W/S or Up/Down Arrow
+        // Get movement input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
         // Normalize diagonal movement
-        movement = movement.normalized;
+        if (movement.magnitude > 1)
+            movement.Normalize();
+
+        // Set animator parameters
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+
+        // Flip character for left movement
+        if (movement.x != 0)
+            transform.localScale = new Vector3(Mathf.Sign(movement.x), 1, 1);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // Move the player
+        rb.linearVelocity = movement * speed;
     }
 }
